99b9cc797882066d7441f0fe05ce80a35f27ec18
John Teye Doku <86769906+johnteye@users.noreply.github.com>
2025-01-30 10:26:14 +0000

    Build Error Fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import Countries from './components/Countries'
-import Country from './components/country'
+import Country from './components/Country'
 
 
 export default function App() {
