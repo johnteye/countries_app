43f91a3cc01717b416c1128ada2d92d8120f8138
John Teye Doku <86769906+johnteye@users.noreply.github.com>
2025-01-30 11:25:29 +0000

    Responsive Single Country design

--- src/components/Country.tsx
+++ src/components/Country.tsx
@@ -1,9 +1,10 @@
 
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams , Link} from "react-router-dom";
 import { useGetCountryByIdQuery } from "../redux/allcountriesApi";
 import { useEffect, useState } from "react";
 import SpinnerComponent from "../components/Spinner"
+
 
 const Country = () => {
   const params = useParams<{ countryName: string }>();
@@ -37,18 +38,22 @@ const Country = () => {
 
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen">
+  
+      <Link to="/" className="absolute top-4 left-4 bg-blue-500 text-white px-4 py-2 rounded">
+        Back
+      </Link>
     
-        <div className="border-2 rounded-3xl h-5/6 w-1/3 p-6 flex flex-col justify-evenly shadow-lg divide-y">
+        <div className="border-2 rounded-3xl h-5/6 w-4/5 md:w-2/3 p-6 flex flex-col justify-evenly shadow-lg divide-y">
           <h1 className="text-center font-sans font-semibold text-3xl">
             Detail Country View
           </h1>
-          <h2 className="font-sans font-medium text-xl">Official Name: {`${countryData?.name?.official}`}</h2>
-          <p className="font-sans font-medium text-xl">Capital: {countryData.capital?.[0] || "N/A"}</p>
-          <p className="font-sans font-medium text-xl">Region: {countryData?.region} </p>
-          <p className="font-sans font-medium text-xl">Subregion: {countryData?.subregion || "N/A"}</p>
-          <p className="font-sans font-medium text-xl">Population: {countryData?.population} </p>
-          <p className="font-sans font-medium text-xl">Area (sq km): {countryData?.area}</p>
-          <p className="font-sans font-medium text-xl"> Languages: {countryData?.languages ? Object.values(countryData.languages).join(", ") : "N/A"} </p>
+          <h2 className="font-sans font-normal text-xl">Official Name: {`${countryData?.name?.official}`}</h2>
+          <p className="font-sans font-normal text-xl">Capital: {countryData.capital?.[0] || "N/A"}</p>
+          <p className="font-sans font-normal text-xl">Region: {countryData?.region} </p>
+          <p className="font-sans font-normal text-xl">Subregion: {countryData?.subregion || "N/A"}</p>
+          <p className="font-sans font-normal text-xl">Population: {countryData?.population} </p>
+          <p className="font-sans font-normal text-xl">Area (sq km): {countryData?.area}</p>
+          <p className="font-sans font-normal text-xl"> Languages: {countryData?.languages ? Object.values(countryData.languages).join(", ") : "N/A"} </p>
         </div>
     
     
